7aa5e30c79301d2dc894e39d1d75db49f45e14d6
Grrraou <datain.alexandre@gmail.com>
2024-09-14 00:18:56 +0200

    Init ts version

---
+++ src/views/CounterView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Counter Page</h1>
+      <p>Count: {{ store.count }}</p>
+      <button @click="store.increment">Increment</button>
+      <router-link to="/">Go to Home</router-link>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { useStore } from '@/stores/useStore';
+  
+  export default defineComponent({
+    setup() {
+      const store = useStore();
+  
+      return { store };
+    },
+  });
+  </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <router-link to="/counter">Go to Counter</router-link>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'HomeView',
+  };
+  </script>
