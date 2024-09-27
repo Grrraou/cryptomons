d832cbd2f9acbbcd099d9b8a7bd9f895f2276834
Grrraou <datain.alexandre@gmail.com>
2024-09-27 03:20:52 +0200

    autosell style

--- src/components/InventorySelling.vue
+++ src/components/InventorySelling.vue
@@ -3,9 +3,9 @@
     <h3>Drag Items to Sell</h3>
     <div id="sell-area" class="sell-area">SELL</div>
     <div id="autosell" v-if="NFTsManager.isCollectionCompleted('azookie')">
-      <div v-for="(checked, index) in ItemManager.getItemStore().autoSell">
-        <input type="checkbox" :checked="checked" @click="toggleAutoSell(index)" :id="`autosell-${index}`">
-        <label>sell {{ index }}</label>
+      <div v-for="(checked, rarity) in ItemManager.getItemStore().autoSell">
+        <input type="checkbox" :checked="checked" @click="toggleAutoSell(rarity)" :id="`autosell-${rarity}`">
+        <label :for="`autosell-${rarity}`">sell <span :class="`autosell-rarity autosell-rarity-${rarity}`"></span></label>
       </div>
     </div>
   </div>
@@ -82,6 +82,49 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
   background-color: #fff;
+}
+
+#autosell input {
+  padding: 8px 12px;
+    font-size: 16px;
+    color: #333;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    appearance: none;
+    margin-bottom: -3px;
+}
+
+.autosell-rarity {
+  height: 12px;
+  width: 12px;
+  display: inline-block;
+}
+
+.autosell-rarity-0 {
+  background-color: rgb(0, 0, 0);
+}
+
+.autosell-rarity-1 {
+  background-color: rgb(157, 157, 157);
+}
+
+.autosell-rarity-2 {
+  background-color: rgb(94, 193, 94);
+}
+
+.autosell-rarity-3 {
+  background-color: rgb(59, 130, 246);
+}
+
+.autosell-rarity-4 {
+  background-color: rgb(168, 113, 193);
+}
+
+.autosell-rarity-5 {
+  background-color: rgb(255, 165, 0);
 }
 </style>
   
